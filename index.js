1e14c12c0daebd758aa2eccff42f9ea67e8b2d8e
Gandharve Solanki <91267904+GandharveSolanki@users.noreply.github.com>
2023-08-08 17:29:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import redis from "redis";
 const app = express();
 const port = process.env.PORT || 3000;
 
-const redisClient = createClient({
+const redisClient = await createClient({
     password: 'swUkW8EvYdWXLPTY7ke8FbBr0ywqSiFb',
     socket: {
         host: 'redis-16490.c15.us-east-1-4.ec2.cloud.redislabs.com',
