53cdca3f7339b3d9fd96d58cc34a37793c4d578a
Gandharve Solanki <91267904+GandharveSolanki@users.noreply.github.com>
2023-08-08 17:29:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,16 @@
 import express from "express";
 import axios from "axios";
-import { createClient } from 'redis';
+import redis from "redis";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Use the Redis client configuration provided by the cloud service
 const redisClient = createClient({
     password: 'swUkW8EvYdWXLPTY7ke8FbBr0ywqSiFb',
     socket: {
         host: 'redis-16490.c15.us-east-1-4.ec2.cloud.redislabs.com',
         port: 16490
     }
-});
-
-redisClient.on("error", (error) => console.error(`Error : ${error}`));
-
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
 });
 
 async function fetchApiData(species) {
@@ -30,10 +23,11 @@ async function fetchApiData(species) {
 
 async function cacheData(req, res, next) {
   const species = req.params.species;
+  let results;
   try {
     const cacheResults = await redisClient.get(species);
     if (cacheResults) {
-      const results = JSON.parse(cacheResults);
+      results = JSON.parse(cacheResults);
       res.send({
         fromCache: true,
         data: results,
@@ -43,39 +37,30 @@ async function cacheData(req, res, next) {
     }
   } catch (error) {
     console.error(error);
-    res.status(404).send("Cache error");
+    res.status(404);
   }
 }
-
 async function getSpeciesData(req, res) {
   const species = req.params.species;
+  let results;
 
   try {
-    const cacheResults = await redisClient.get(species);
-    if (cacheResults) {
-      const results = JSON.parse(cacheResults);
-      res.send({
-        fromCache: true,
-        data: results,
-      });
-    } else {
-      const results = await fetchApiData(species);
-      if (results.length === 0) {
-        throw "API returned an empty array";
-      }
-      await redisClient.set(species, JSON.stringify(results), {
-        EX: 180,
-        NX: true,
-      });
-
-      res.send({
-        fromCache: false,
-        data: results,
-      });
+    results = await fetchApiData(species);
+    if (results.length === 0) {
+      throw "API returned an empty array";
     }
+    await redisClient.set(species, JSON.stringify(results), {
+      EX: 180,
+      NX: true,
+    });
+
+    res.send({
+      fromCache: false,
+      data: results,
+    });
   } catch (error) {
     console.error(error);
-    res.status(404).send("Data error");
+    res.status(404).send("Data unavailable");
   }
 }
 
